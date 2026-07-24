3a60a181a82930d24e158690bc30c66fae36cfb8
sathwik-0910 <mailforsathwik@gmail.com>
2026-07-25 00:49:47 +0530

    Time: 2 ms (99.54%), Space: 49.5 MB (17.52%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -3,13 +3,8 @@ class Solution {
         int []arr = new int[nums.length];
         int max=-1;
         for(int i=nums.length-1;i>=0;i--){
-            if(i==nums.length-1){
-                arr[i]=max;
-                max=Math.max(nums[i],max);
-            }else{
-                arr[i]=max;
+           arr[i]=max;
                 max=Math.max(max,nums[i]);
-            }
         }
         return arr;
     }
