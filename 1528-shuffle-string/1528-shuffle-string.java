d0f92520491ff263a5206e732175170764afe9af
sathwik-0910 <mailforsathwik@gmail.com>
2026-08-15 01:00:46 +0530

    Time: 1 ms (64.19%), Space: 45.3 MB (87.34%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,9 +1,11 @@
 class Solution {
-    public String restoreString(String s, int[] in) {
-        char []a = new char[s.length()];
-        for(int i=0;i<in.length;i++){
-            a[in[i]]=s.charAt(i);
+    public String restoreString(String s, int[] p) {
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < p.length; i++) {
+            sb.setCharAt(p[i], s.charAt(i));
         }
-        return new String(a);
+
+        return sb.toString();
     }
 }
